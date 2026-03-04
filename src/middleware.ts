af0eceb8e402f624d65f894bbf615e4c13655fc5
Wesley Eduardo <111645247+wesleyeduardodev@users.noreply.github.com>
2026-03-04 20:02:45 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,11 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    salt: "authjs.session-token",
+  });
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
